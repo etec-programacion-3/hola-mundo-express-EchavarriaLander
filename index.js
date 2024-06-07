06e33a4c6dd2ce7c22d71c89408144766701c221
Lander <l.echavarria@alumno.etec.um.edu.ar>
2024-06-07 12:22:02 -0300

    saludojs

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/saludo/:nombre',(req,res)=>{
+  const nombre = req.params.nombre;
+  res.send(`Hola ${nombre}, ¿Como estas?`);
+})
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
